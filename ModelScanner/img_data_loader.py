8b4ae6779f9389fd0aab8eee8123039de342f414
Unknown <hizitefery@gmail.com>
2017-12-03 17:06:50 +0100

    Finished normals calculation.

--- ModelScanner/img_data_loader.py
+++ ModelScanner/img_data_loader.py
@@ -1,30 +1,29 @@
-import pygame, sys
-from PIL.ImageOps import flip
-from pygame.examples.midi import input_main
-from pygame.locals import *
+import os
+import sys
+import time
+
+import numpy
+import pygame
 from OpenGL.GL import *
 from OpenGL.GLU import *
-from pygame.constants import *
 from PIL import Image
-import os
-import time
-import math
-
-green2 = [0.0, 0.5, 0.0, 0.0]
+from PIL.ImageOps import flip
+from pygame.constants import *
 
 
-def cross(a, b):
-    c = [a[1] * b[2] - a[2] * b[1],
-         a[2] * b[0] - a[0] * b[2],
-         a[0] * b[1] - a[1] * b[0]]
-
-    return c
+def normalize_v3(arr):
+    # Normalize a numpy array of 3 component vectors shape=(n,3)
+    lens = numpy.sqrt(arr[:, 0] ** 2 + arr[:, 1] ** 2 + arr[:, 2] ** 2)
+    arr[:, 0] /= lens
+    arr[:, 1] /= lens
+    arr[:, 2] /= lens
+    return arr
 
 
 class OBJ:
     def __init__(self, filename, swapyz=True):
-        has_mtl = False
-        """Loads a Wavefront OBJ file. """
+        # has_mtl = False
+        # Loads a Wavefront OBJ file
         self.vertices = []
         self.normals = []
         self.texcoords = []
@@ -32,9 +31,11 @@ class OBJ:
 
         material = None
         for line in open(filename, "r"):
-            if line.startswith('#'): continue
+            if line.startswith('#'):
+                continue
             values = line.split()
-            if not values: continue
+            if not values:
+                continue
             if values[0] == 'v':
                 v = list(map(float, values[1:4]))
                 if swapyz:
@@ -45,71 +46,74 @@ class OBJ:
                 if swapyz:
                     v = v[0], v[2], v[1]
                 self.normals.append(v)
-            elif values[0] == 'vt':
-                self.texcoords.append(map(float, values[1:3]))
-            elif values[0] in ('usemtl', 'usemat'):
-                material = values[1]
-
             elif values[0] == 'f':
                 face = []
                 texcoords = []
                 norms = []
-                normal_vertices = []
                 for v in values[1:]:
                     w = v.split('/')
-                    face.append(int(w[0]))
-                    if len(w) >= 2 and len(w[1]) > 0:
-                        texcoords.append(int(w[1]))
-                    else:
-                        texcoords.append(0)
-                    if len(w) >= 3 and len(w[2]) > 0:
-                        norms.append(int(w[2]))
-                    else:
-                        norms.append(0)
-                # Per vertex
-                    # Use cross-products to calculate the face normals for the triangles surrounding a given vertex,
-                # add them together, and normalize.
-                vertex1 = [(round(n, 4)) for n in self.vertices[face[0] - 1]]
-                # print 'Vertex1 = ' + str(vertex1)
-                vertex2 = [(round(n, 4)) for n in self.vertices[face[1] - 1]]
-                # print 'Vertex2 = ' + str(vertex2)
-                vertex3 = [(round(n, 4)) for n in self.vertices[face[2] - 1]]
-                # print 'Vertex3 = ' + str(vertex3)
-                vector1 = [vertex2[0] - vertex1[0], vertex2[1] - vertex1[1], vertex2[2] - vertex1[2]]
+                    face.append(int(w[0]) - 1)
+                    # if len(w) >= 2 and len(w[1]) > 0:
+                    #     texcoords.append(int(w[1]))
+                    # else:
+                    #     texcoords.append(0)
+                    # if len(w) >= 3 and len(w[2]) > 0:
+                    #     norms.append(int(w[2]))
+                    # else:
+                    #     norms.append(0)
 
-                vector2 = [vertex3[0] - vertex1[0], vertex3[1] - vertex1[1], vertex3[2] - vertex1[2]]
-                vector3 = [(round(n, 4)) for n in cross(vector2, vector1)]
+                    self.faces.append((face, norms, texcoords, material))
 
-                # norms.append(normalize(vector3))
-                print("cross product: " + str(vector3))
+        n_vertices = numpy.array(self.vertices)
+        n_faces = numpy.array([row[0] for row in self.faces])
 
-                # print self.vertices[10]
-                # print "Vertex: " + self.vertices[face[0]]
+        # Create a zeroed array with the same type and shape as our vertices i.e., per vertex normal
 
-                self.faces.append((face, norms, texcoords, material))
-                # print(self.faces)
+        norm = numpy.zeros(n_vertices.shape, dtype=n_vertices.dtype)
+        # Create an indexed view into the vertex array using the array of three indices for triangles
+        tris = n_vertices[n_faces]
+        # Calculate the normal for all the triangles, by taking the cross product of the vectors v1-v0, and v2-v0 in
+        #  each triangle
+        n = numpy.cross(tris[::, 1] - tris[::, 0], tris[::, 2] - tris[::, 0])
+        # n is now an array of normals per triangle. The length of each normal is dependent the vertices,
+        # we need to normalize these, so that our next step weights each normal equally.
+        normalize_v3(n)
+        # Now we have a normalized array of normals, one per triangle, i.e., per triangle normals.
+        # But instead of one per triangle (i.e., flat shading), we add to each vertex in that triangle,
+        # the triangles' normal. Multiple triangles would then contribute to every vertex, so we need to
+        # normalize again afterwards.
+        norm[n_faces[:, 0]] += n
+        norm[n_faces[:, 1]] += n
+        norm[n_faces[:, 2]] += n
+        normalize_v3(norm)
+
         self.gl_list = glGenLists(1)
         glNewList(self.gl_list, GL_COMPILE)
         glFrontFace(GL_CCW)
-        for face in self.faces:
-            vertices, normals, texture_coords, material = face
+        glBegin(GL_TRIANGLES)
+        for face in n_faces:
+            t_normal = norm[face]
+            t_vertice = n_vertices[face]
 
-            glBegin(GL_POLYGON)
-            for i in range(len(vertices)):
-                if normals[i] > 0:
-                    glNormal3fv(self.normals[normals[i] - 1])
-                glVertex3fv(self.vertices[vertices[i] - 1])
-            glEnd()
+            glNormal3fv(t_normal[0].tolist())
+            glVertex3fv(t_vertice[0].tolist())
+            glNormal3fv(t_normal[1].tolist())
+            glVertex3fv(t_vertice[1].tolist())
+            glNormal3fv(t_normal[2].tolist())
+            glVertex3fv(t_vertice[2].tolist())
+
+        glEnd()
         glDisable(GL_TEXTURE_2D)
         glEndList()
+
+        # def calculate_normals(self,face):
 
 
 def main():
     target_fps = 60
     pygame.init()
     viewport = (1600, 900)
-    hx = viewport[0] / 2
-    hy = viewport[1] / 2
+
     screen = pygame.display.set_mode(viewport, OPENGL | DOUBLEBUF | GL_DEPTH)
 
     glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE)
@@ -124,7 +128,7 @@ def main():
     glShadeModel(GL_SMOOTH)  # most obj files expect to be smooth-shaded
 
     input_dir = sys.argv[1]
-    output_dir = '/home/hyzi/Documents/results/'
+    output_dir = sys.argv[2]
     filenames = os.listdir(input_dir)
     filenames.sort()
 
@@ -139,10 +143,7 @@ def main():
     glEnable(GL_DEPTH_TEST)
     glMatrixMode(GL_MODELVIEW)
 
-    rx, ry = (0, 0)
-    tx, ty = (0, 0)
-    zpos = 5
-    rotate = move = False
+
     i = 0
     iteration = 0
     index = 0
@@ -162,15 +163,10 @@ def main():
 
         # After 12 screenshot change object
         # RENDER OBJECT
-        glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, green2)
-        glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, green2)
         glTranslate(0, 0, - 15)
         glRotate(-60, 1, 0, 0)
-        # glRotate(30,)
         glRotate(i, 0, 0, 1)
 
-        # glRotate(ry, 1, 0, 0)
-        # glRotate(rx, 0, 1, 0)
         glScale(15.0, 15.0, 15.0)
         glCallList(obj.gl_list)
         if i % 30 == 0:
